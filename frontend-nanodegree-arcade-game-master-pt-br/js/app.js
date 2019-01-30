f8b569e441720edc87bfe6b6934dd03809c63437
hiratafs <tatihirata@outlook.com>
2019-01-30 15:08:33 -0300

    For loop para gerar os enemies

--- frontend-nanodegree-arcade-game-master-pt-br/js/app.js
+++ frontend-nanodegree-arcade-game-master-pt-br/js/app.js
@@ -18,6 +18,15 @@ class Enemy  {
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
+
+    checkCollision() {
+        if(this.x < player.x + player.width &&
+            this.x + this.width > player.x &&
+            this.y < player.y + player.height &&
+            this.y + this.height > player.y) {
+            console.log("Bateu!");
+        }
+    }
 }
 
 //Classe para construir o objeto Player
@@ -57,11 +66,10 @@ class Player {
    
     }
 
-    checkCollision() {
-        //if(this.x)
-    };
-
+    
 }
+
+
 
 
 // Represente seus objetos como instâncias.
@@ -70,29 +78,34 @@ class Player {
 
 var allEnemies = [];
 
-var enemy_a = new Enemy(0, 50, velocidade);
+/* var enemy_a = new Enemy(0, 50, velocidade);
 var enemy_b = new Enemy(0, 130, velocidade);
 var enemy_c = new Enemy(0, 210, velocidade);
 
 allEnemies.push(enemy_a);
 allEnemies.push(enemy_b);
-allEnemies.push(enemy_c); 
+allEnemies.push(enemy_c);  */
+
+for(var i = 0; i < 3; i++) {
+    var velocidade = 101 + Math.floor(Math.random() * 400);
+    var coordY = [50, 130, 210];
+    var posicaoY = coordY[Math.floor(Math.random() * 3)]
+    allEnemies.push(new Enemy(0, posicaoY, velocidade));
+}
 
 
-var velocidade = 101 + Math.floor(Math.random() * 400)
+//var velocidade = 101 + Math.floor(Math.random() * 400)
 
 //Variáveis em array para a posição X do player
 var posicaoX = [0, 101, 202, 303, 404];
 
 // Variáveis em array para a posição Y dos enemies
-var posicaoY = [50, 130, 210];
+//var posicaoY = [50, 130, 210];
 
 // Determina de maneira randômica a posição X do player 
 var player_x = posicaoX[Math.floor(Math.random() * 4)];
 //var enemy_y = posicaoY[Math.floor(Math.random() * 2)];
 var player = new Player(player_x);
-
-
 
 // Isto reconhece cliques em teclas e envia as chaves para seu
 // jogador. método handleInput(). Não é preciso mudar nada.
