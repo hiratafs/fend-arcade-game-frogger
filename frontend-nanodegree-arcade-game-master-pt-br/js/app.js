65b8fa37ad24352eaf121daadca84ae1fbf0e5bf
hiratafs <tatihirata@outlook.com>
2019-01-30 14:45:24 -0300

    Adicionados comentários ao código

--- frontend-nanodegree-arcade-game-master-pt-br/js/app.js
+++ frontend-nanodegree-arcade-game-master-pt-br/js/app.js
@@ -6,7 +6,6 @@ class Enemy  {
         this.y = y;
         this.x = x;
         this.velocidade = velocidade;
-        
     }
 
     update(dt) {
@@ -19,9 +18,9 @@ class Enemy  {
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-   }
+}
 
-
+//Classe para construir o objeto Player
 class Player {
     constructor(x, y = 400) {
         this.sprite = "images/char-boy.png";
@@ -37,6 +36,7 @@ class Player {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
+    // Lógica da movimentação do player pelo canvas
     handleInput(tecla) {
         this.tecla = tecla;
         if(tecla === "left" && this.x > 2) {
@@ -54,34 +54,44 @@ class Player {
         } else {
             return;
         }
-
-        
+   
     }
 
+    checkCollision() {
+        //if(this.x)
+    };
+
 }
 
-function checkCollision() {
-    
-}
 
 // Represente seus objetos como instâncias.
 // Coloque todos os objetos inimgos numa array allEnemies
 // Coloque o objeto do jogador numa variável chamada jogador.
 
 var allEnemies = [];
-var velocidade = 100 + Math.floor(Math.random() * 400)
-var posicaoX = [0, 101, 202, 303, 404];
-var posicaoY = [50, 130, 210];
-var player_x = posicaoX[Math.floor(Math.random() * 4)]
-//var enemy_y = posicaoY[Math.floor(Math.random() * 2)];
-var player = new Player(player_x);
+
 var enemy_a = new Enemy(0, 50, velocidade);
 var enemy_b = new Enemy(0, 130, velocidade);
 var enemy_c = new Enemy(0, 210, velocidade);
 
 allEnemies.push(enemy_a);
 allEnemies.push(enemy_b);
-allEnemies.push(enemy_c);
+allEnemies.push(enemy_c); 
+
+
+var velocidade = 101 + Math.floor(Math.random() * 400)
+
+//Variáveis em array para a posição X do player
+var posicaoX = [0, 101, 202, 303, 404];
+
+// Variáveis em array para a posição Y dos enemies
+var posicaoY = [50, 130, 210];
+
+// Determina de maneira randômica a posição X do player 
+var player_x = posicaoX[Math.floor(Math.random() * 4)];
+//var enemy_y = posicaoY[Math.floor(Math.random() * 2)];
+var player = new Player(player_x);
+
 
 
 // Isto reconhece cliques em teclas e envia as chaves para seu
@@ -98,5 +108,8 @@ document.addEventListener('keyup', function(e) {
 });
 
 function reachingWater() {
-    //if
+    if(this.y === -10) {
+        console.log("Você venceu!");
+    }
 }
+
